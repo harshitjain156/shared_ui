1cb6385c69edffefb7ffe6baccb2955a297e8d8b
Harshit jain <harshit2020cs198@abesit.edu.in>
2023-11-22 15:43:15 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	lib/main.dart

--- lib/list.dart
+++ lib/list.dart
@@ -173,7 +173,6 @@ class _BottomSheetDataState extends State<BottomSheetData> {
                   decoration: BoxDecoration(borderRadius:BorderRadius.circular(10) ,color: Colors.grey.shade200,border:Border.all(color: Colors.grey.shade600)),
 
                   child: TextField(
-
                         decoration: InputDecoration(
 
                           border: OutlineInputBorder(
